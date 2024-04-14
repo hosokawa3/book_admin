68843d66e499cb5fde16d480fb75cc3752f0cfaa
hosokawa3 <clover091039@gmail.com>
2024-04-14 13:47:21 +0000

    [Add]User_model,profiles_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
+  resources :publishers
+  resource :profile, only: [:show, :edit, :update]
 end
